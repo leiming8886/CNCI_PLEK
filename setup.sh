bc3b1a0cd338102eb71c1fd2b3a19190192fea93
leiming8886 <48576287+leiming8886@users.noreply.github.com>
2019-04-10 16:59:12 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ tar zvxf PLEK.1.2.tar.gz
 cd PLEK.1.2
 python PLEK_setup.py
 cd ../
-upzip fileName.zip
+unzip fileName.zip
 cd CNCI-master
 chmod ugo+x  twoBitToFa
 unzip libsvm-3.0.zip
@@ -12,7 +12,4 @@ cd libsvm-3.0
 make
 cd ../../
 #source setup.sh
-<<<<<<< HEAD
 #matplotlib-venn
-=======
->>>>>>> 7e0eb251e058e81aa098e1ab6463a87a151b216b
