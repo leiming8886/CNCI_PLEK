c4b4a5c801707b96164d3ade83fc54544d2ddbc3
leiming8886 <48576287+leiming8886@users.noreply.github.com>
2019-04-19 09:51:20 +0800

    Update INCP.py

--- INCP.py
+++ INCP.py
@@ -178,5 +178,5 @@ for key in intersect_array(gi_p,gi_c):
     #print(key + '\tnonconding\n')
 union.close()
 inter.close()
-#run gtf pass : python PLEK_CNCI.py -f K510.gtf -p 6 -g -d /home/lmjiang/software/CNCI/hg38.2bit 
-#run fasta pass: python PLEK_CNCI.py -f K510.fa -p 6
+#run gtf pass : python INCP.py -f K510.gtf -p 6 -g -r /home/lmjiang/software/CNCI/hg38.2bit 
+#run fasta pass: python INCP.py -f K510.fa -p 6
